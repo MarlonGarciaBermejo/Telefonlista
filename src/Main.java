21b383de47594db107f75453b0b1dff0ab236f8a
Marlon <yhma22.garcma@folkuniversitetet.nu>
2022-10-13 19:33:16 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> telefonLista = new HashMap<>();
+
+        telefonLista.put("Greta","543 3431 1533");
+        telefonLista.put("Karin","634 2341 2344");
+        telefonLista.put("Kevin","324 5430 1222");
+
+    }
+}
